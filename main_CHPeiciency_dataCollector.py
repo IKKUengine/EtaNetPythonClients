ac41d340d901c7ceb8d6556d3338cf38d79cc02d
Ferhat Aslan <ferhat.aslan@hs-karlsruhe.de>
2018-09-19 19:01:12 +0200

    feat: request, calculate and output of gas mass flow value

--- main_CHPeiciency_dataCollector.py
+++ main_CHPeiciency_dataCollector.py
@@ -3,6 +3,8 @@ import serial
 from threading import Thread
 from tkinter import *
 import RPi.GPIO as GPIO
+import Adafruit_ADS1x15
+
 
 
 #ToDo: Überblicksdokumentation
@@ -16,7 +18,18 @@ class guiApplication(Frame):
     textPower = 'Hallo IKKUengine!'
     textSignal = ''
     
-
+    # Choose a gain of 1 for reading voltages from 0 to 4.09V.
+    # Or pick a different gain to change the range of voltages that are read:
+    #  - 2/3 = +/-6.144V
+    #  -   1 = +/-4.096V
+    #  -   2 = +/-2.048V
+    #  -   4 = +/-1.024V
+    #  -   8 = +/-0.512V
+    #  -  16 = +/-0.256V
+    # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+    GAIN = 1
+        
+    
 
     ser = serial.Serial (
         port='/dev/ttyS0', #Open RPI buit-in serial port
@@ -30,6 +43,14 @@ class guiApplication(Frame):
     def __init__(self, master=None):
         
         #Hardware Init
+        
+        # Create an ADS1115 ADC (16-bit) instance.
+        self.adc = Adafruit_ADS1x15.ADS1115()
+
+        # Note you can change the I2C address from its default (0x48), and/or the I2C
+        # bus by passing in these optional parameters:
+        #adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+        
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(12, GPIO.OUT)
@@ -59,12 +80,12 @@ class guiApplication(Frame):
     
         #t=Thread(target=self.masurementRunning, args=()).start()
         #self.ser.write(str.encode('FORM:PH L1\n'))
-        self.ser.write(str.encode('VOLT:RMS:AC?\n'))
+        #self.ser.write(str.encode('VOLT:RMS:AC?\n'))
         #ser.write(str.encode('CURR:RMS:AC?\n'))
         #ser.write(str.encode('POW:FAC:AC?\n'))       
-        data1 = self.ser.read(40) #Read 10 characters from serial port to data
+        #data1 = self.ser.read(10) #Read 10 characters from serial port to data
         self.ser.write(str.encode('POW:ACT:AC?\n'))
-        data2 = self.ser.read(40)
+        #data2 = self.ser.read(15)
 
         #ser.write(str.encode('POW:ACT:AC? FORM:PH L1?\n'))
         #data3 = ser.read(40)
@@ -75,15 +96,15 @@ class guiApplication(Frame):
         #ser.write(str.encode('POW:ACT:AC? FORM:PH L3?\n'))
         #data5 = ser.read(40)
 
-        self.ser.write(str.encode('FORM:PH L3\n'))
+        #self.ser.write(str.encode('FORM:PH L3\n'))
 
         self.ser.write(str.encode('VOLT:RMS:AC?\n'))
         #ser.write(str.encode('CURR:RMS:AC?\n'))
         #ser.write(str.encode('POW:FAC:AC?\n'))       
-        data4 = self.ser.read(40) #Read 10 characters from serial port to data
+        data = self.ser.read(22) #Read 10 characters from serial port to data
 
-        self.ser.write(str.encode('POW:ACT:AC?\n'))
-        data5 = self.ser.read(40)
+        #self.ser.write(str.encode('POW:ACT:AC?\n'))
+        #data5 = self.ser.read(10)
 
         #ser.write(str.encode('POW:ACT:AC? FORM:PH L1?\n'))
         #data3 = ser.read(40)
@@ -100,12 +121,30 @@ class guiApplication(Frame):
         #ser.write(str.encode('POW:ACT:AC?\n'))
         #data3 = ser.read(100)
 
-        self.infoText['text'] = data1 + str.encode(";") + data2 + str.encode(";") + data4 + str.encode(";") + data5
+        self.infoText['text'] =  data
 
         self.ser.close
     
     def requestGasMassFlow(self):
-        self.infoText['text'] = 'Gas Mass Flow muss noch implementiert werden...'
+        # Read all the ADC channel values in a list.
+        values = [0]*4
+        #for i in range(4):
+        # Read the specified ADC channel using the previously set gain value.
+        values[0] = self.adc.read_adc(0, gain=self.GAIN)
+        #values[1] = adc.read_adc(1, gain=GAIN)
+        #values[2] = adc.read_adc(2, gain=GAIN)
+        #values[3] = adc.read_adc(3, gain=GAIN)
+        # Note you can also pass in an optional data_rate parameter that controls
+        # the ADC conversion time (in samples/second). Each chip has a different
+        # set of allowed data rate values, see datasheet Table 9 config register
+        # DR bit values.
+        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        # Each value will be a 16 bit signed integer value depending on the
+        # ADC (ADS1115 = 16-bit).
+        
+        #Calculation and calibration of the gas fuel flow
+        fuelflow = (values[0]) / (2 * 3276.8) * 4.2 * 0.046166667
+        self.infoText['text'] = str(fuelflow)
 
     def requestHeatMeterHotWater(self):
         self.infoText['text'] = 'Heat Meter 2 (Warmwasser) muss noch implementiert werden...'
@@ -121,10 +160,7 @@ class guiApplication(Frame):
             self.signalText['text'] = 'CHP is OFF'
         else:
             self.signalText['text'] = 'CHP is ON'
-
         GPIO.output(12, not GPIO.input(12)) 
-       # self.onOff_button = Button(master, text = 'Toggle', width=20, command = self.relais).place(relx=0.5, rely = 0.5, anchor= CENTER)
-
 
     def requestWeatherData(self):
         self.infoText['text'] = 'Weather Data muss noch implementiert werden...'
@@ -135,8 +171,6 @@ class guiApplication(Frame):
         
     def masurementRunning(self):
         self.infoText['text'] = 'Die Messung läuft! Bitte warten...'
-
-
 
 root = Tk()
 menu_bar = Menu(root)
