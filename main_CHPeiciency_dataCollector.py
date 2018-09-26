144d09f688327d16cf252c35f98108a6279951ac
Ferhat Aslan <ferhat.aslan@hs-karlsruhe.de>
2018-09-26 14:23:25 +0200

    refactor: Reading M-Bus frame of all data

--- main_CHPeiciency_dataCollector.py
+++ main_CHPeiciency_dataCollector.py
@@ -1,11 +1,10 @@
 import time
 import serial
-from threading import Thread
+
 from tkinter import *
 import RPi.GPIO as GPIO
 import Adafruit_ADS1x15
 import meterbus
-
 
 #ToDo: Überblicksdokumentation
 #why threading? -> if one of the requests in work and cannot continue
@@ -159,13 +158,51 @@ class guiApplication(Frame):
        "\x00\x00\x00\x00\x00\x00\x02\x7A\x25\x00\x02\x78" \
        "\x25\x00\x3A\x16"
         
-        data2 = "ich will sehen" \
-                "was passiert"
+        data2 = "\x68\x71\x71\x68\x08\x02\x72\x49\x01\x19" \
+       "\x10\x49\x6A\x88\x04\x8B" \
+       "\x00\x00\x00\x0C\x78\x49\x01\x19\x10\x04" \
+       "\x06\x01\x00\x00\x00\x82" \
+       "\x04\x6C\x41\x21\xC2\x84\x00\x6C\xFF\xFF" \
+       "\x84\x04\x06\x00\x00\x00" \
+       "\x00\xC4\x84\x00\x06\x00\x00\x00\x80\x82" \
+       "\x0A\x6C\x41\x29x84\x0A" \
+       "\x06\x00\x00\x00\x00\x04" \
+       "\x13\x18\x02\x00\x00\x02\x59\xDB\x07\x02" \
+       "\x5D\xB2\x07\x02\x61\x29\x00\x04" \
+       "\x2D\x00\x00\x00\x00\x04\x3B\x00" \
+       "\x00\x00\x00\x04\x6D\x11\x0D\x58\x29\x04" \
+       "\x26\x2F\x18\x00\x00\x02" \
+       "\xFD\x17\x00\x00\x1F\x49\x16"
+        
+        data3 = "\x68\x71\x71\x68\x08\x02\x72\x49\x01\x19\x10\x49\x6A\x88\x04\xBE" \
+       "\x00\x00\x00\x0C\x78\x49\x01\x19\x10\x04\x06\x01\x00\x00" \
+       "\x00\x82\x04\x6C\x41\x21\xC2\x84\x00\x6C\xFF\xFF\x84\x04\x06\x00" \
+       "\x00\x00\x00\xC4\x84\x00\x06\x00\x00\x00\x80\x82\x0A\x6C\x41\x29\x84\x0A" \
+       "\x06\x00\x00\x00\x00\x04\x13\x18\x02\x00\x00\x02\x59\xA4\x08\x02\x5D\x16\x09" \
+       "\x02\x61\x00\x00\x04\x2D\x00\x00\x00\x00\x04\x3B\x00\x00\x00\x00\x04\x6D\x1A" \
+       "\x0A\x59\x29\x04\x26\x44\x18\x00\x00\x02\xFD\x17\x00\x00\x1F\x9F\x16"
+        
+        
+        telegram = meterbus.load(data3)
+        self.infoText['text'] =  str(telegram.records[0].interpreted) + "\n" \
+        + str(telegram.records[1].interpreted) + "\n" \
+        + str(telegram.records[2].interpreted) + "\n" \
+        + str(telegram.records[3].interpreted) + "\n" \
+        + str(telegram.records[4].interpreted) + "\n" \
+        + str(telegram.records[5].interpreted) + "\n" \
+        + str(telegram.records[6].interpreted) + "\n" \
+        + str(telegram.records[7].interpreted) + "\n" \
+        + str(telegram.records[8].interpreted) + "\n" \
+        + str(telegram.records[9].interpreted) + "\n" \
+        + str(telegram.records[10].interpreted) + "\n" \
+        + str(telegram.records[11].interpreted) + "\n" \
+        + str(telegram.records[12].interpreted) + "\n" \
+        + str(telegram.records[13].interpreted) + "\n" \
+        + str(telegram.records[14].interpreted) + "\n" \
+        + str(telegram.records[15].interpreted) + "\n" \
+        + str(telegram.records[16].interpreted) 
 
-        telegram = meterbus.load(data)
-        self.infoText['text'] = str(telegram.records[10].parsed_value)
-        self.infoText['text'] = str(data2)
-
+        
     def requestHeatMeterHeatingWater(self):
         self.infoText['text'] = 'Heat Meter 1 (Heizwasser) muss noch implementiert werden...'
 
